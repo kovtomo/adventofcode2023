3b3c9a755c0a86363fd313f5618f25e912bda0b0
kovtomo <kov.tomo@gmail.com>
2023-12-24 14:45:09 +0100

    1B comments added

--- day01B.py
+++ day01B.py
@@ -23,6 +23,10 @@ def firstLastDigit(inputItem):
     
 def wordToNumber(inputItem):
 
+    """" Looking for the first index in the <inputItem> string 
+            of each occurance of each number written with letters 
+            and create a dict of the index, values pairs """    
+
     wordNumber = {
         "one":"1",
         "two":"2",
@@ -35,64 +39,88 @@ def wordToNumber(inputItem):
         "nine":"9"
     } 
     
-    justNumber = {
-        "one":1,
-        "two":2,
-        "three":3,
-        "four":4,
-        "five":5,
-        "six":6,
-        "seven":7,
-        "eight":8,
-        "nine":9
-    } 
-    
-    leftnessRankingDict = {}
+    numberWithLetterDict = {}
     for i in wordNumber:
         tempIndexList = [x.start() for x in re.finditer(i, inputItem)]
         if tempIndexList:
             for j in tempIndexList:
-                leftnessRankingDict[j] = i
-                
-    leftnessRankingDictNumbers = {}
-    for i in justNumber.values():
+                numberWithLetterDict[j] = i
+        
+    """" Looking for the first index in the <inputItem> string 
+            of each occurance of each number written as a number 
+            and create a dict of the index, values pairs """ 
+            
+    numberList = list(range(1,10))
+    
+    numberWithNumberDict = {}
+    for i in numberList:
         tempIndexList = [x.start() for x in re.finditer(str(i), inputItem)]
         if tempIndexList:
             for j in tempIndexList:
-                leftnessRankingDictNumbers[j] = i
+                numberWithNumberDict[j] = i
     
-    if leftnessRankingDict:
-        if leftnessRankingDictNumbers:
-            leftnessRankingDict.update(leftnessRankingDictNumbers)
+    """" In case both above created dict exists, 
+            create a single dict and sort it by key 
+        
+         Then choose the first and last item by key in the sorted dict. These
+         are the first and last occurances of numbers in <inputItem> string,
+         written either by letters or as a number
+         
+         If both the first and last number in the <inputItem> string are 
+         written by letters, then modify the positional index of the
+         last item (decrease by the lenght of the first number written as a
+         word) as it will change after the replacement of the first number
+         written with letters (the <inputItem> string will be shorter after the 
+         first iteration of replacement)
+         
+             eg. nine54gfdgeight -> 0-nine; 10-eight
+             
+                 After replacing "0-nine", the initial string will be shorter
+                 So "10-eight"" is no longer true: 954gfdgeight -> 7-eight
+         
+        Finally cut the <inputItem> string by half at the start index of the 
+        number written with letters (which is the key of the our dict) 
+        which we want to replace to make sure we replace the number written 
+        with letters at the right position starting from left to right and
+        going to replace only the first occurance. At the end we just concat
+        back the two halves and we are finished with the replacement """ 
             
-        leftnessRankingDict = dict(sorted(leftnessRankingDict.items()))
+    if numberWithLetterDict:
+        
+        allNumbersRankedDict = dict()
+        allNumbersRankedDict.update(numberWithLetterDict)
+
+        if numberWithNumberDict:
+            allNumbersRankedDict.update(numberWithNumberDict)
+            
+        allNumbersRankedDict = dict(sorted(allNumbersRankedDict.items()))
         
         elementMin = (
-            min(leftnessRankingDict),
-            leftnessRankingDict[min(leftnessRankingDict)]
+            min(allNumbersRankedDict),
+            allNumbersRankedDict[min(allNumbersRankedDict)]
         )
         elementMax = (
-            max(leftnessRankingDict),
-            leftnessRankingDict[max(leftnessRankingDict)]
+            max(allNumbersRankedDict),
+            allNumbersRankedDict[max(allNumbersRankedDict)]
         )
-        leftnessRankingDictRestr = dict([elementMin, elementMax])
+        minMaxDict = dict([elementMin, elementMax])
         
-        isItString = [isinstance(x, str) for x in leftnessRankingDictRestr.values()]       
+        isItString = [isinstance(x, str) for x in minMaxDict.values()]       
         if isItString.count(True) == 2:
-            newIndex = max(leftnessRankingDictRestr) - \
-                len(leftnessRankingDictRestr[min(leftnessRankingDictRestr)])
-            leftnessRankingDictRestr[newIndex] = leftnessRankingDictRestr.pop(
-                max(leftnessRankingDictRestr)
+            newIndex = max(minMaxDict) - \
+                len(minMaxDict[min(minMaxDict)])
+            minMaxDict[newIndex] = minMaxDict.pop(
+                max(minMaxDict)
             )
             
-        for i in leftnessRankingDictRestr:
-            if isinstance(leftnessRankingDictRestr[i], str) \
-                and leftnessRankingDictRestr[i] in inputItem:
+        for i in minMaxDict:
+            if isinstance(minMaxDict[i], str) \
+                and minMaxDict[i] in inputItem:
                 stringFirstHalf = inputItem[:i]
                 stringSecondHalf = inputItem[i:]
                 stringSecondHalf = stringSecondHalf.replace(
-                    leftnessRankingDictRestr[i], 
-                    wordNumber[leftnessRankingDictRestr[i]], 
+                    minMaxDict[i], 
+                    wordNumber[minMaxDict[i]], 
                     1
                 )
                 inputItem = stringFirstHalf + stringSecondHalf
